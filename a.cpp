eab61e75fef862ebf46c0260f87a85480ff6faaf
Aditya-Rupda <106980743+Aditya-Rupda@users.noreply.github.com>
2024-06-19 09:32:27 +0530

    Create a.cpp

---
+++ a.cpp
@@ -0,0 +1,17 @@
+//A
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define endl "\n"
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    ll t; cin >> t;
+    while(t--){
+        string a,b;
+        cin >> a>> b;
+        swap(a[0],b[0]);
+        cout << a << " " << b << endl;
+    }
+    return 0;
+}
